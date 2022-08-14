9830d09e95bf737cc7b88a92eb02059d14bb53ba
Suzon999 <ashikurrahamansuzon346112.com>
2022-08-14 20:19:19 +0600

    2 option event handler add js

---
+++ js/event.js
@@ -0,0 +1,9 @@
+
+// button event handelar option -1
+function makeaRed() {
+    document.body.style.backgroundColor = 'red';
+}
+// button event handelar option -2
+document.getElementById("make-pink").addEventListener("click", function (e) {
+    document.body.style.backgroundColor = 'pink';
+})
